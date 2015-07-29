3b24a9f0c62dcfc5a9d7392da3481a9f93fb28dd
Paul Solt <PaulSolt@gmail.com>
2015-07-29 11:57:54 -0400

    Add a comment and code
    Testing our first code change

--- DemoGithub/DemoGithub/ViewController.swift
+++ DemoGithub/DemoGithub/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // comment
+        
+        var x = 10
     }
 
     override func didReceiveMemoryWarning() {
